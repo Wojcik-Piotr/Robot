08a3718cc25d8b1c49e3a3f4a6962b11485f7e51
Piotr Wójcik <bajuschi38@gmail.com>
2019-01-31 05:11:04 +0100

    clean

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Platform platform = new Platform();
         Robot robot = new Robot(platform);
-        robot.run("PMLPMMMLPMLPMML");
-        robot.run("PLPLPLPLPLPLPLPL");
         robot.run(args[0]);
     }
 }
